622719e7b81fda475dcb0ddf825d6a87b9c60745
curufuin <48504750+curufuin@users.noreply.github.com>
2019-04-20 11:55:00 -0700

    Update connector.py
    Fixing some issues connecting to open wifi.

--- connector.py
+++ connector.py
@@ -1,6 +1,8 @@
 import wifi
 import math
 import time
+import os
+import netifaces
 from wireless import Wireless
 from collections import OrderedDict
 
@@ -8,7 +10,7 @@ from collections import OrderedDict
 class Connector():
 
     def __init__(self):
-        self.interface = "wlan0"
+        self.interface = "wlan1"
         self.wifilist = []
         self.ratings = OrderedDict()
         #blacklist
@@ -17,8 +19,10 @@ class Connector():
         #passwords
         self.passwords = {}
         self.passwordsFile = "./passwords.txt"
-        #wifi objects
+        #cell object
         self.cells = None
+        self.wifiConfigFile = "/etc/wpa_supplicant/wpa_supplicant.conf"
+        #wpa connector object
         self.wireless = Wireless()
 
     def search(self):
@@ -33,43 +37,86 @@ class Connector():
         return sig * qual
 
     def sortDict(self, dict1):
-        values = sorted(dict1.values())
+        #sort dictionary by values.
+        values = sorted(dict1.values(), reverse=True)
+        print("values")
+        print(values)
         newdict = OrderedDict()
-        for key,value in dict1.items():
-            val1 = values.pop()
-            if value == val1:
-                newdict[key] = value
+        for val1 in values:
+		for key,value in dict1.items():
+		    if value == val1:
+		        newdict[key] = value
         return newdict
 
     def scan(self):
+        #find available wireless networks
         for cell in self.search():
             self.ratings[str(cell.ssid)] = self.rate(cell)
+        time.sleep(4)
+        print(self.ratings)
+        for cell in self.search():
+            self.ratings[str(cell.ssid)] = self.rate(cell)
+        time.sleep(4)
+        print(self.ratings)
+        for cell in self.search():
+            self.ratings[str(cell.ssid)] = self.rate(cell)
+        print(self.ratings)
         for ssid in self.blacklist:
             self.ratings.pop(ssid, None)
         self.ratings = self.sortDict(self.ratings)
         return self.ratings
 
     def conn(self, ssid):
-        for cell in self.cells:
-                if cell.ssid == ssid:
-                    print("connecting to: " + str(ssid))
-                    if cell.encrypted is False:
-                        try:
-                            print("connecting to open wifi")
-                            return self.wireless.connect(ssid, "")
-                        except:
-                            print("couldn't connect")
-                            return False
+        if self.wireless.current() == None:
+		for cell in self.cells:
+		        if cell.ssid == ssid:
+		            print("connecting to: " + str(ssid))
+                            #connect to insecure wifi
+		            if cell.encrypted is False:
+		                try:
+		                    print("\nconnecting to open wifi")
 
-                    elif cell.encrypted is True:
-                        print("encrypted, looking for PW")
-                        try:
-                            pw = self.passwords[ssid]
-                            print(pw)
-                            return self.wireless.connect(ssid, pw)
-                        except:
-                            print("couldn't connect'")
-                            return False
+                                    f = open(self.wifiConfigFile, 'r')
+                                    lines = f.readlines()
+                                    ssidExists = False
+                                    for line in lines:
+                                        if 'ssid="' + ssid in line:
+                                            ssidExists = True
+                                    f.close()
+                                    if not ssidExists:
+                                        cmd = "network={\n" + 'ssid="' + ssid + '"\n' + "key_mgmt=NONE\n" + "}\n"
+                                        f = open(self.wifiConfigFile, 'a')
+                                        f.write(cmd)
+                                        f.close()
+                                    cmd = "sudo service wpa_supplicant restart"
+                                    os.system(cmd)
+                                    time.sleep(3)
+                                    cmd = "sudo iwconfig " + self.interface + " essid '" + cell.ssid + "'"
+                                    os.system(cmd)
+                                    time.sleep(3)
+                                    cmd = "sudo dhclient " + self.interface 
+                                    os.system(cmd)
+                                    time.sleep(3)
+                                    # only return true if connected and acquired an ip address
+                                    if self.wireless.current() is not None and netifaces.ifaddresses(c.interface)[2] is not None:
+		                        return True
+                                    else:
+                                        return False
+		                except:
+		                    return False
+                            #connect to encrypted wireless networks
+		            elif cell.encrypted is True:
+		                print("encrypted, looking for PW")
+		                try:
+		                    pw = self.passwords[ssid]
+		                    print(pw)
+		                    return self.wireless.connect(ssid, pw)
+		                except:
+		                    print("couldn't connect'")
+		                    return False
+        else:
+            print("already connected to: " + str(self.wireless.current()))
+            return self.wireless.current()
 
     def makeConnection(self):
         #go through list of wifi SSID's by best connection and connect.
@@ -104,8 +151,7 @@ c = Connector()
 c.readBlacklist()
 c.readPW()
 
-
-#get quality of network
+#get quality of each available network
 print(c.scan())
 
 #make best connection
