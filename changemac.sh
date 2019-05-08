0687bba05e5eefb071dc61d66c0ab2b9f6e16df8
curufuin <48504750+curufuin@users.noreply.github.com>
2019-05-07 17:45:36 -0700

    Update changemac.sh
    updating script for changing hostname to work better and break less things.

--- changemac.sh
+++ changemac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-hostname=$(/usr/bin/shuf -n 1 ./hostnames.txt)
+currentHostname=$(hostname)
+hostname=$(/usr/bin/shuf -n 1 /home/pi/bin/hostnames.txt)
 
 
-/sbin/ifconfig wlan1 down && /usr/bin/macchanger -r wlan1 && /sbin/ifconfig wlan1 up && /bin/echo $hostname > /etc/hostname && /bin/hostname $hostname && /usr/sbin/service networking restart
+/sbin/ifconfig wlan1 down && /usr/bin/macchanger -r wlan1 && /sbin/ifconfig wlan1 up && /bin/echo $hostname > /etc/hostname && /bin/hostname $hostname && /bin/sed -i "s/${currentHostname}/${hostname}/g" /etc/hosts && /usr/sbin/service networking restart
